324bc7760c464ed5c3e9de6ecd8e721b0e4a4b29
dlinch <dlinch33@gmail.com>
2015-11-14 14:44:12 -0700

    added function shuffler based on a stack overflow response, preliminary jquery function to take submitted form data and push to array

--- main.js
+++ main.js
@@ -2,10 +2,33 @@ var randomNumber = function (){
   return Math.floor(Math.random()*33)
 }
 
+var shuffleArray = function(array){
+  var currentIndex= array.length, temporaryValue, randomIndex;
+  while (currentIndex !==0 ){
+    randomIndex= Math.floor(Math.random()*currentIndex);
+    currrentIndex -= 1;
+    temporaryValue = array[currentIndex];
+    array[currentIndex]=array[randomIndex];
+    array[randomIndex]= temporaryValue;
+  }
+  return array;
+}
+var bracketItems = [];
 
 $('document').ready(function(){
-$('button').on('click', function(){
-  var $('#primaryInput')
+
+$('#form').on('submit', function(event){
+  event.preventDefault();
+  var data = $('#form:input');
+  bracketItems.push(data);
+  console.log(data);
 })
 
 })
+
+
+var shuffledArray = shuffleArray(bracketItems);
+
+for(i=0; i<32; i++){
+  $('#bucket-'+i).append(shuffledArray[i])
+}
