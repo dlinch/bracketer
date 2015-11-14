f02a4b686360407b8b485a94a8704d303a87e3f2
dlinch <dlinch33@gmail.com>
2015-11-14 13:51:28 -0700

    preliminary random number function written, some initial testing divs and an input area initialized. Started on the jQuery functionality to assign input to an array.

---
+++ main.js
@@ -0,0 +1,11 @@
+var randomNumber = function (){
+  return Math.floor(Math.random()*33)
+}
+
+
+$('document').ready(function(){
+$('button').on('click', function(){
+  var $('#primaryInput')
+})
+
+})
